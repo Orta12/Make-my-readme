8541535b48d1f6c7dd940ac1c252d76b160d3e64
Dani Boii <105583437+Orta12@users.noreply.github.com>
2022-07-15 17:55:41 +0530

    All files in your hand

---
+++ index.js
@@ -0,0 +1,43 @@
+projectname = () => {
+    var project_name = document.getElementById("project_name").value;
+    var project_name_ = document.getElementById("project_").innerText = project_name;
+}
+
+description = () => {
+    var description = document.getElementById("description").value;
+    var description_ = document.getElementById("description_").innerText = description;
+}
+installation = () => {
+    var installation = document.getElementById("installation").value;
+    var installation_ = document.getElementById("installation_").innerText = installation;
+}
+install = () => {
+    var install = document.getElementById("install").value;
+    var package_ = document.getElementById("package_").innerText = install;
+}
+link = () => {
+    var link = document.getElementById("link").value;
+    var link_ = document.getElementById("link_").innerText = link;
+}
+linkuap = () => {
+    var linkup = document.getElementById("namedlink").value;
+    var linkmy = document.getElementById("linkmy").href = linkup;
+}
+usage = () => {
+    var usage = document.getElementById("usage").value;
+    var usage_ = document.getElementById("Usage_").innerText = usage;
+}
+describe = () => {
+    var describe = document.getElementById("describe").value;
+    var describe_  = document.getElementById("usage_").innerText = describe;
+}
+
+Contributing = () => {
+    var Contributing = document.getElementById("contributing").value
+    var contributing_ = document.getElementById("contributing_").innerText = Contributing;
+}
+contributing_des = () => {
+    var contributing_des = document.getElementById("contributing_des").value;
+    var contributing_des_ = document.getElementById("contributing_des_").innerText = contributing_des;
+}
+
